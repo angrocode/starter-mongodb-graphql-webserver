8f4810a901ee0312f8af4c4add03ad6c194f1206
angro <mail@angro.org>
2023-01-23 17:36:19 +0300

    Update utils.js

--- common/utils.js
+++ common/utils.js
@@ -19,15 +19,3 @@ export const futureDateObj = () => new Date(32503680000000)
 export const tokenExpiresDateObj = expires => new Date(Date.now() + (expires * 1000))
 export const currentMsec = () => Date.now()
 export const currentSec = () => Math.round(Date.now() / 1000)
-
-export const locationParser = location => {
-  let s
-  if (location.includes('#')) s = '#'
-  else if (location.includes('?')) s = '?'
-  else s = '/'
-
-  const [u, q] = location.split(s)
-  let r; const urn = (r = u.split('/').filter(Boolean).map(v => decodeURIComponent(v))).length > 0 ? r : null
-  if (!q) return {urn, query: null}
-  else return {urn, body: Object.fromEntries(decodeURIComponent(q).split('&').filter(Boolean).map(v => v.split('=')))}
-}
